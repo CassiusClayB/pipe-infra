03d0589eb39976a242a925eb92d01a1d77a04952
Cassius Clay Filho <cassiussilvafilho@gmail.com>
2024-01-16 21:58:51 -0300

    Update main_test.go

--- tests/main_test.go
+++ tests/main_test.go
@@ -17,19 +17,5 @@ func TestTerraformExample(t *testing.T) {
 	// Ao finalizar os testes, destrua os recursos criados
 	defer terraform.Destroy(t, options)
 
-	// Execute `terraform init` e `terraform apply`. Falha no teste se houver erro
-	terraform.InitAndApply(t, options)
-
-	// Verifique se os recursos esperados foram criados
-	s3BucketName := terraform.Output(t, options, "s3_bucket_name")
-	assert.NotEmpty(t, s3BucketName, "O nome do bucket S3 não deve estar vazio.")
-
-	vpcID := terraform.Output(t, options, "vpc_id")
-	assert.NotEmpty(t, vpcID, "O ID da VPC não deve estar vazio.")
-
-	subnetIDs := terraform.OutputList(t, options, "subnet_ids")
-	assert.NotEmpty(t, subnetIDs, "A lista de IDs das Subnets não deve estar vazia.")
-
-	ec2InstancePublicIP := terraform.Output(t, options, "ec2_instance_public_ip")
-	assert.NotEmpty(t, ec2InstancePublicIP, "O endereço IP público da instância EC2 não deve estar vazio.")
+	
 }
